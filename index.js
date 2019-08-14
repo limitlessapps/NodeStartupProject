1af5ff2b649ad67273c6ffd81460405d2dbefb71
zachee <zacheeishimwe@gmail.com>
2019-08-14 12:23:27 +0200

    updates

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const userRoutes = require("./routes/UsersRoutes");
+
+//=====================================================================App use
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(bodyParser.json({ limit: '10mb', extended: true }));
+app.use(cors());
+ 
+
+
+
+//===================================================================== port 5000
+app.set('port', Number(process.env.PORT || 5000));
+const server = app.listen(app.get('port'), function () {
+    console.log('Listening on ' + app.get('port'));
+});
+
+
+//===================================================================== routes
+app.use("/api", userRoutes);
+
+
+
+//=====================================================================localhost connection
+mongoose.connect('mongodb://localhost:27017/myapp',
+    {
+        useCreateIndex: true,
+        useNewUrlParser: true
+    }
+    , function () { 
+        console.log("App connected success")
+    });
